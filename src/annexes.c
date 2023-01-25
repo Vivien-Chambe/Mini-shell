f2a0f45c371eabd63a48a9682379cd91c225fd7e
neiviv-dev <vivien.chambe@live.com>
2023-01-25 19:07:26 +0100

    init

---
+++ src/annexes.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+void color_noir (int bold) {
+    printf("\033[%d;30m", !!bold);
+}
+
+void color_rouge (int bold) {
+    printf("\033[%d;31m", !!bold);
+}
+
+void color_vert (int bold) {
+    printf("\033[%d;32m", !!bold);
+}
+
+void color_jaune (int bold) {
+    printf("\033[%d;33m", !!bold);
+}
+
+void color_bleu (int bold) {
+    printf("\033[%d;34m", !!bold);
+}
+
+void color_violet (int bold) {
+    printf("\033[%d;35m", !!bold);
+}
+
+void color_cyan (int bold) {
+    printf("\033[%d;36m", !!bold);
+}
+
+void color_blanc (int bold) {
+    printf("\033[%d;37m", !!bold);
+}
+
+void color_reset () {
+    printf("\033[0m");
+}
+
+void init(){
+    color_vert(1);
+    printf("             / '-'  /\n");
+    printf("            ;       ;\n");
+    printf("         /'-|       |-' |\n");
+    printf("        |   |_______P   |\n");
+    printf("        |   '-------'   /\n");
+    printf("         '.___.....___.'\n");
+    printf("             | ;  : ;|\n");
+    printf("            _|;__;__.|_\n");
+    printf("           |     Y     |\n");
+    printf("           |___.'^'.___|\n");
+    printf("             |_  ;  _|      .--.\n");
+    printf(" .--.        {^^^^^^^}     /;   |\n");
+    printf("/   ;|       {       }    | ;   |\n");
+    printf("|  ; |       {       }    |     |\n");
+    printf("|;   |       ;-.__.'|    |:  ; |\n");
+    printf("| ;  |      |;  ;   |_____/ ;  |\n");
+    printf("|   '.'-----'      ' -_   .'   /\n");
+    printf("|  '.   - _  ' ;  ;  _  -    .'\n");
+    printf(" '.   -     - ;  ;   .------`\n");
+    printf("   `--------.      ; |\n");
+    printf("            |;  ,    |\n");
+    printf("            |;  ,    |\n");
+    color_reset();
+    printf("\nBienvenue dans le shell\n\n");
+}
---
+++ src/annexes.h
@@ -0,0 +1,10 @@
+void init();
+void color_noir (int bold);
+void color_rouge (int bold);
+void color_vert (int bold);
+void color_jaune (int bold);
+void color_bleu (int bold);
+void color_violet (int bold);
+void color_cyan (int bold);
+void color_blanc (int bold);
+void color_reset ();
